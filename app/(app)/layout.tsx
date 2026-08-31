b505abb20eaafc41ce68c5dc832a4040cda96a91
rencitizen <ren.citizen.ai@gmail.com>
2026-08-31 21:07:27 +0900

    Apply LIFE_OS app shell across modules

--- app/(app)/layout.tsx
+++ app/(app)/layout.tsx
@@ -17,12 +17,14 @@ export default async function AppLayout({
   if (!user) redirect('/login')
 
   return (
-    <div className="flex h-screen">
+    <div className="flex min-h-screen bg-[#f3f6fb] dark:bg-[#0f172a]">
       <AppSidebar />
-      <div className="flex-1 flex flex-col overflow-hidden">
+      <div className="flex min-w-0 flex-1 flex-col">
         <AppHeader />
-        <main className="flex-1 overflow-y-auto p-4 pb-20 lg:p-6 lg:pb-6">
-          {children}
+        <main className="flex-1 overflow-y-auto px-4 py-4 pb-24 lg:px-6 lg:py-6 lg:pb-8">
+          <div className="mx-auto w-full max-w-6xl">
+            {children}
+          </div>
         </main>
       </div>
       <BottomNav />
